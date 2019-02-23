10bb76e933c4059c92ec38265739df27c37fcf5d
veselinn16 <veselinn16@gmail.com>
2019-02-23 18:49:15 +0200

    Include theme provider :sparkles:

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux'; // the react-redux export allowing us to subscribe automatically to store updates
 import store from '../store';
 import DevTools from './DevTools';
+import StyledContainer from '../components/StyledContainer'; // the container for the styled components
+import { GlobalStyle } from '../injectGlobalStyles'; // the global styles
+
+import { ThemeProvider } from 'styled-components'; // for making the theme object accessible to all styled components
+import theme from '../constants/theme'; // theme object holding all 'CSS variables'
 
 const Root = () => {
     return(
         <Provider store={store}>
-            <div>
-                <h1>Hello, Root Component</h1>
-                <DevTools />
-            </div>
+            <ThemeProvider theme={theme}>
+                <StyledContainer>
+                    <GlobalStyle/>
+                    <DevTools />
+                </StyledContainer>
+            </ThemeProvider>
         </Provider>
     )
 }
