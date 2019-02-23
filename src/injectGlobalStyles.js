374a3b10abba0cd99802e4bbf2f5832f647ba5cd
veselinn16 <veselinn16@gmail.com>
2019-02-23 18:49:48 +0200

    Add base styles :art:

---
+++ src/injectGlobalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'; // helper function returning component
+
+export const GlobalStyle = createGlobalStyle`
+    *::before,
+    *::after,
+    * {
+        box-sizing: border-box;
+    }
+
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: "Roboto", sans-serif;
+    }
+
+    #root {
+        min-height: 100vh;
+    }
+`;
+
+    
+
+
