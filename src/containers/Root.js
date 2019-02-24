d94212d456dfc8e77eefa151e9628e379328d22f
veselinn16 <veselinn16@gmail.com>
2019-02-24 21:45:01 +0200

    Specify components to render on different routes :wrench:

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux'; // the react-redux export allowing us to subscribe automatically to store updates
+import { BrowserRouter } from 'react-router-dom';
 import store from '../store';
 import DevTools from './DevTools';
 import StyledContainer from '../components/StyledContainer'; // the container for the styled components
@@ -8,11 +9,16 @@ import { GlobalStyle } from '../injectGlobalStyles'; // the global styles
 import { ThemeProvider } from 'styled-components'; // for making the theme object accessible to all styled components
 import theme from '../constants/theme'; // theme object holding all 'CSS variables'
 
+import App from '../components/App';
+
 const Root = () => {
     return(
         <Provider store={store}>
             <ThemeProvider theme={theme}>
                 <StyledContainer>
+                    <BrowserRouter>
+                        <App/>
+                    </BrowserRouter>
                     <GlobalStyle/>
                     <DevTools />
                 </StyledContainer>
