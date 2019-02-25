aec264e0ad7a4edece605291cf74ad593affaf36
veselinn16 <veselinn16@gmail.com>
2019-02-25 16:05:23 +0200

    Render movies list :sparkles:

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Icon from './Icon';
+import StyledHeader from './StyledHeader';
+import StyledHeaderTitle from './StyledHeaderTitle';
+import StyledHorizontalScroll from './StyledHorizontalScroll';
+import Movie from './Movie';
+import StyledFooter from './StyledFooter';
+import StyledLargeButton from './StyledLargeButton';
 
-const Movies = () => {
-    return (
-        <div>Movies</div>
-    )
-}
+import { movies } from '../staticData';
+
+
+const Movies = () => (
+    <Fragment>
+        <StyledHeader>
+            <Icon icon="bars" text="help" />
+            <StyledHeaderTitle>Movie Recommendations</StyledHeaderTitle>
+            <Icon icon="search" />
+        </StyledHeader>
+        <StyledHorizontalScroll>
+            {movies.map(movie => (
+                <Movie
+                    key={movie.id}
+                    poster={movie.poster}
+                    duration={movie.duration}
+                    name={movie.name}
+                    year={movie.year}
+                />
+            ))}
+        </StyledHorizontalScroll>
+        <StyledFooter>
+            <StyledLargeButton>Get Recommended Movies</StyledLargeButton>
+        </StyledFooter>
+    </Fragment>
+)
 
 export default Movies;
