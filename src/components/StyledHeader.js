12dc7af6008f896b97e2fa02a2c8997fa3742aa6
veselinn16 <veselinn16@gmail.com>
2019-02-25 15:59:27 +0200

    Add Styled Header component :construction:

---
+++ src/components/StyledHeader.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    height: 70px;
+    display: flex;
+    align-items: center;
+    color: #8b9095;
+    font-size: .8rem;
+`;
+
+export default StyledHeader;
