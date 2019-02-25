f145904a771687ee6b57f3291530966ac6813b7d
veselinn16 <veselinn16@gmail.com>
2019-02-25 16:03:50 +0200

    Add variables for poster height and width :wrench:

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,7 +1,9 @@
 //theme object holding all 'CSS variables'
 const theme = {
     primaryBg: '#333852',
-    secondaryBg: '#282a31'
+    secondaryBg: '#282a31',
+    posterWidth: '127px',
+    posterHeight: '191px'
 };
 
 export default theme;
