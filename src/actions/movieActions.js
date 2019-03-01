ba15d2860343e3c890d84cb18812b87dfad2e5b5
veselinn16 <veselinn16@gmail.com>
2019-03-01 10:40:01 +0200

    Create get movies action creator :wrench:

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -1,19 +1,28 @@
 import { createAction } from 'redux-actions';
 import { apiPayloadCreator } from '../utils/appUtils';
-import { API } from '../constants/actionTypes';
+import { API, SET_MOVIES } from '../constants/actionTypes';
+import { normalize, schema } from 'normalizr';
 
-const getMoviesAC = () => createAction(API, apiPayloadCreator); // retruns action
-
+const getMoviesAC = () => createAction(API, apiPayloadCreator); // retruns an action creator function
 //calls the action creator and specifies the required properties of the network request
 export const getMovies = () => getMoviesAC({
     url: '/vcvx0',
-    onSuccess: setMovies
+    onSuccess: setMovies,
 })
 
-// let f = createAction(API, apiPayloadCreator);
-// console.log(f);
+console.log(getMovies());
 
 function setMovies(movies) {
+    // normalize the list of movie objects into an object with keys being the movie objects
+    const movieSchema = new schema.Entity('movies'); // defines the movie object to be an entity
+    const movieListSchema = new schema.Array(movieSchema); // defines that the data from the server is an array
+    const normalizedData = normalize(movies, movieListSchema); // normalizes the data
+    
     console.log(movies); // result from server
-    return { type: '' }; // dummy aciton. Returned because action creator is expected to return an action
+    
+    // action that has the normalized data as its payload
+    return { 
+        type: SET_MOVIES,
+        payload: normalizedData.entities.movies
+    };
 }
