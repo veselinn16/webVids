2888295bcaee295cb7a2cf6fa00ff3dbf63ac11d
veselinn16 <veselinn16@gmail.com>
2019-03-01 10:37:50 +0200

    Add action types for toast message creation :wrench:

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,7 +1,9 @@
 // action type variables used for consistency across files
 
 export const GET_MOVIES = 'GET_MOVIES';
+export const SET_MOVIES = 'SET_MOVIES';
 export const SELECT_MOVIE = 'SELECT_MOVIE';
 export const API = 'API';
 export const API_START = 'API_START';
 export const API_END = 'API_END';
+export const SHOW_TOAST = 'SHOW_TOAST';
