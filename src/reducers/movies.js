af6e092b071f33ba8fe3f37f39b29beeb8a8a2de
veselinn16 <veselinn16@gmail.com>
2019-03-01 11:01:30 +0200

    Replace false action type string :bug:

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,9 +1,9 @@
 import { handleActions } from "redux-actions"; // this yields a reducer
-import { GET_MOVIES } from '../constants/actionTypes';
+import { SET_MOVIES } from '../constants/actionTypes';
 
 export default handleActions(
     {
-        [GET_MOVIES]: (state, action) => action.payload // GET_MOVIES has square brackets, because it's a variable
+        [SET_MOVIES]: (state, action) => action.payload // GET_MOVIES has square brackets, because it's a variable
         // this action sets the movies returned by the network request to be the movies in the Redux store
     },
     {}
