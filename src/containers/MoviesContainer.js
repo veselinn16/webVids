46bc2683a0c53524d52e58fbb654c2c0e3e88ad9
veselinn16 <veselinn16@gmail.com>
2019-03-06 20:50:27 +0200

    Render loader when request is in process :sparkles:

--- src/containers/MoviesContainer.js
+++ src/containers/MoviesContainer.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 
 import { getMovies } from '../actions/movieActions';
 import Movies from '../components/Movies';
+import { GET_MOVIES } from '../constants/label';
 
 // this container acts as a bridge between the Movies component and whatever props are made available via the connect(). Essentially, this container communicates with Redux's state and passes the props to the Movies component
 const MoviesContainer = props => <Movies {...props}/>;
 
 const mapStateToProps = state => ({
-    movies: _.values(state.movies) // converts the object's key values into an array
+    movies: _.shuffle(_.values(state.movies)), // converts the object's key values into an array,
+    loading: state.isLoading[GET_MOVIES] // for displaying loader in API requests
 }); // this will be accessible via props
 
 export default connect(
