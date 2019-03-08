362dbd620b295ea187c3780e861df8ec3c3af44d
veselinn16 <veselinn16@gmail.com>
2019-03-08 12:03:44 +0200

    Set movie id in state upon clicking of movie :wrench:

--- src/reducers/selectMovie.js
+++
@@ -1,9 +0,0 @@
-import { handleActions } from 'redux-actions';
-import { SELECT_MOVIE } from '../constants/actionTypes';
-
-export default handleActions(
-    {
-        [SELECT_MOVIE]: (state, action) => state
-    },
-    1
-)
