2062039b33497800483668f00927a3a6c109ecbe
veselinn16 <veselinn16@gmail.com>
2019-03-08 12:01:38 +0200

    Wire up selectMovie action creator to MovieDetails component :wrench:

---
+++ src/containers/MovieDetailsContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import MovieDetails from '../components/MovieDetails';
+import { selectMovie } from '../actions/movieActions';
+
+const MovieDetailsContainer = props => <MovieDetails {...props} />
+
+const mapStateToProps = state => ({
+    selectedMovie: state.movies[state.selectedMovie] // get the selected movie from the state
+});
+
+export default connect(
+    mapStateToProps,
+    { selectMovie }
+)(MovieDetailsContainer);
