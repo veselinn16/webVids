a0d5bdb9638b9c6c5f9c4c448d185959e53ebe80
veselinn16 <veselinn16@gmail.com>
2019-03-08 12:08:15 +0200

    Add additional styling to movie poster :art:

--- src/components/StyledPoster.js
+++ src/components/StyledPoster.js
@@ -1,9 +1,20 @@
 import styled from 'styled-components';
+import { devices } from '../utils/styledUtils';
 
+// check if an element has isExpanded prop to apply different styling
 const StyledPoster = styled.img`
-    width: ${props => props.theme.posterWidth};
-    height: ${props => props.theme.posterHeight};
+    width: ${(props => props.isExpanded ? 
+        props.theme.posterWidthExpanded : 
+        props.theme.posterWidth)};
+    height: ${(props => props.isExpanded ? 
+        props.theme.posterHeightExpanded : 
+        props.theme.posterHeight)};
     border-radius: 4px;
+    ${devices.md`
+        position: ${props => (props.isExpanded ? 'absolute' : 'initial')};
+        top: ${props => (props.isExpanded ? '-10%' : 'initial')};
+        left: ${props => (props.isExpanded ? '-6%' : 'initial')};
+    `}
 `;
 
 export default StyledPoster;
