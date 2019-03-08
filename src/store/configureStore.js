5a55ddf0d9ae695831e829b031c37924bdd9d52a
veselinn16 <veselinn16@gmail.com>
2019-03-08 12:04:59 +0200

    Add toast middleware to store :wrench:

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,13 +4,14 @@ import logger from 'redux-logger';
 import api from '../middleware/api';
 import DevTools from '../containers/DevTools';
 import { persistStore } from 'redux-persist';
+import toastMiddleware from '../middleware/toasts';
 
 const configureStore = initialState => { // if initialState is passed, add it to the store
     const store = createStore(
         rootReducer,
         initialState,
         compose( // used for adding multiple store enhancers
-            applyMiddleware(logger, api), 
+            applyMiddleware(api, toastMiddleware, logger), 
             DevTools.instrument() // adds the DevTools via the instrument prop
         )
     );
