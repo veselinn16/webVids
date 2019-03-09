9b3e0b77f96114794b3a0f0401525f41e62b3d6c
veselinn16 <veselinn16@gmail.com>
2019-03-09 10:21:36 +0200

    Add Root component for dev purposes :wrench:

---
+++ src/containers/Root.dev.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Provider } from 'react-redux'; // the react-redux export allowing us to subscribe automatically to store updates
+import { BrowserRouter } from 'react-router-dom';
+import DevTools from './DevTools';
+import StyledContainer from '../components/StyledContainer'; // the container for the styled components
+import { GlobalStyle } from '../injectGlobalStyles'; // the global styles
+
+import { ThemeProvider } from 'styled-components'; // for making the theme object accessible to all styled components
+import theme from '../constants/theme'; // theme object holding all 'CSS variables'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistedStore } from '../store';
+
+import App from '../components/App';
+
+const Root = () => (
+    <Provider store={store}>
+        <PersistGate persistor={persistedStore} loading={null}>
+            <ThemeProvider theme={theme}>
+                <StyledContainer>
+                    <BrowserRouter>
+                        <App/>
+                    </BrowserRouter>
+                    <GlobalStyle/>
+                    <DevTools />
+                    <ToastContainer/>
+                </StyledContainer>
+            </ThemeProvider>
+        </PersistGate>
+    </Provider>
+)
+
+export default Root;
